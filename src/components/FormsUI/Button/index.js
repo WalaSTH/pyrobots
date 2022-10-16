3e9ab7585b72c037fc479e5fa86587ab32b85e0a
MartinPiloni <maartinpiloni@gmail.com>
2022-10-16 02:57:25 -0300

    Create match form

--- src/components/FormsUI/Button/index.js
+++ src/components/FormsUI/Button/index.js
@@ -1,21 +1,19 @@
-import React from "react";
 import { Button } from "@mui/material";
 import { useFormikContext } from "formik";
 
 export default function ButtonWrapper({ children, ...otherProps }) {
   const { submitForm } = useFormikContext();
-  const handleSubmit = (e) => {
-    e.preventDefault();
+
+  function handleSubmit() {
     submitForm();
-  };
+  }
 
   const configButton = {
+    ...otherProps,
     variant: "contained",
     color: "primary",
     fullWidth: true,
-    type: "submit",
     onClick: handleSubmit,
-    ...otherProps,
   };
 
   return <Button {...configButton}>{children}</Button>;
