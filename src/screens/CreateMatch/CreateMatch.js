28643b6ff2ff74a1df14b8232f6076c327582ecc
MartinPiloni <maartinpiloni@gmail.com>
2022-10-17 01:17:01 -0300

    Status response handling

--- src/screens/CreateMatch/CreateMatch.js
+++ src/screens/CreateMatch/CreateMatch.js
@@ -3,6 +3,10 @@ import { Grid, Container, Box, Typography, Card } from "@mui/material";
 import * as Yup from "yup";
 import Textfield from "../../components/FormsUI/Textfield";
 import Button from "../../components/FormsUI/Button";
+import Snackbar from "../../components/FormsUI/Snackbar";
+import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const initial_form_state = {
   matchName: "",
@@ -11,6 +15,9 @@ const initial_form_state = {
   maxPlayers: "4",
   numberOfGames: "",
   numberOfRounds: "",
+  creator: "1", //TODO: Get creator id from backend
+  robot_id: "1", //TODO: Get robot id from backend
+  robotName: "",
 };
 
 const form_validation = Yup.object().shape({
@@ -43,9 +50,24 @@ const form_validation = Yup.object().shape({
     .positive()
     .max(10000)
     .required("Required"),
+  robotName: Yup.string().required("Required"),
 });
 
 export default function CreateGame() {
+  // const navigate = useNavigate(); TODO: Redirect to lobby
+
+  const [open, setOpen] = useState(false);
+  const [body, setBody] = useState("");
+  const [severity, setSeverity] = useState("");
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <Box
@@ -61,8 +83,37 @@ export default function CreateGame() {
             ...initial_form_state,
           }}
           validationSchema={form_validation}
-          onSubmit={(values) => {
-            console.log(values);
+          onSubmit={async (values) => {
+            const params = new URLSearchParams();
+            params.append("name", values.matchName);
+            params.append("min_players", values.minPlayers);
+            params.append("max_players", values.maxPlayers);
+            params.append("gamesPerMatch", values.numberOfGames);
+            params.append("rounds", values.numberOfRounds);
+            params.append("robot_id", 1);
+            params.append("creator", 2);
+            params.append("password", values.password);
+
+            const response = await axios.post(
+              "https://634ab9a333bb42dca409da46.mockapi.io/api/matches",
+              params
+            );
+            if (response.status === 201) {
+              console.log(response.status);
+              setOpen(true);
+              setSeverity("success");
+              setBody("Match Created!");
+            } else if (response.status === 400) {
+              console.log(response.status);
+              setOpen(true);
+              setSeverity("error");
+              setBody("Selected robot doesnt belong to the user");
+            } else if (response.status === 401) {
+              console.log(response.status);
+              setOpen(true);
+              setSeverity("error");
+              setBody("A match with the same name already exists");
+            }
           }}
         >
           <Form>
@@ -128,6 +179,15 @@ export default function CreateGame() {
                   />
                 </Grid>
 
+                <Grid item xs={12} textAlign="center">
+                  <Typography>Choose your robot!</Typography>
+                  <Textfield
+                    name="robotName"
+                    label="Robot Name"
+                    autoComplete="off"
+                  />
+                </Grid>
+
                 <Grid item xs={12}>
                   <Button>Create match</Button>
                 </Grid>
@@ -135,6 +195,14 @@ export default function CreateGame() {
             </Card>
           </Form>
         </Formik>
+        {open && (
+          <Snackbar
+            open={open}
+            body={body}
+            severity={severity}
+            handleClose={handleClose}
+          />
+        )}
       </Box>
     </Container>
   );
