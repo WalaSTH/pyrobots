416cb0401bebb652033e88f2290ab1a6b03d53fc
Gino C <ginocipolletti@gmail.com>
2022-10-17 12:32:26 -0300

    Fixed error messages.

--- app.py
+++ app.py
@@ -27,13 +27,13 @@ def match_creation(match_data: TempMatch):
     if (match_data.robot_id > check_robot_quantity()):
         raise HTTPException (
             status_code=404,
-            detail="No robot with such ID."
+            detail="No robot with such ID"
         )
 
     if (match_data.creator > check_user_quantity()):
         raise HTTPException (
             status_code=404,
-            detail="No user with such ID."
+            detail="No user with such ID"
         )
 
     if (match_data.password == None):
@@ -41,14 +41,14 @@ def match_creation(match_data: TempMatch):
 
     if (check_robot_ownership(match_data.robot_id, match_data.creator)):
         raise HTTPException (
-            status_code=400,
-            detail='That robot does not belong to that user.'
+            status_code=409,
+            detail=f'Robot {match_data.robot_id} does not belong to that user'
         )
 
     if (check_match_name_exists(match_data.name)):
         raise HTTPException (
             status_code=409,
-            detail='A match with this name already exists.'
+            detail='A match with this name already exists'
         )
 
     match_id = create_match(
