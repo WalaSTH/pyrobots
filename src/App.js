be66b61b6e92fd18fe8849968c822e9c67bbc833
MartinPiloni <maartinpiloni@gmail.com>
2022-10-17 23:39:55 -0300

    Routes and render of the added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./screens/Home/Home";
+import { Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Routes from "./routes";
+import Navbar from "./components/Navbar/Navbar";
+import Leftbar from "./components/Leftbar/Leftbar";
 
 export default function App() {
+  const navigate = useNavigate();
+
+  const [token, setToken] = useState(null);
+
+  const handleLogin = () => {
+    setToken(1);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+  };
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Navbar token={token} navigate={navigate} />
+      <Grid container>
+        {token && (
+          <Grid item xs={2} order={1}>
+            <Leftbar navigate={navigate} handleLogout={handleLogout} />
+          </Grid>
+        )}
+        <Grid item xs={token ? 10 : 12} order={2}>
+          <Routes
+            token={token}
+            setToken={setToken}
+            navigate={navigate}
+            handleLogin={handleLogin}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
