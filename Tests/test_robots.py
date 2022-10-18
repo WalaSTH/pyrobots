70077afce1a498be8c42605940eda02ba2c230a0
WalaSTH <macielsalguero@gmail.com>
2022-10-18 01:03:19 -0300

    Testing files

--- Tests/test_robots.py
+++ Tests/test_robots.py
@@ -1,3 +1,4 @@
+from fastapi import File, UploadFile
 from fastapi.testclient import TestClient
 from Database.Database import *
 from starlette.middleware.cors import CORSMiddleware
@@ -9,16 +10,29 @@ import string
 
 client = TestClient(app)
 
-# Creating Robot 
+# Creating Robot no avatar
 
 def test_create_robot():
     new_robot_upl = {
         "robot_name": (get_random_string_lower(5)),
-        "code": (get_random_string_lower(5)),
-        "avatar": (get_random_string_lower(5)),
         "creator": 1
     }
-    response = client.post("/robot/create", json = new_robot_upl)
+    code = open("Tests/files/dummybot.py", "rb")
+    response = client.post("/robot/create", params = new_robot_upl, files={"code":code})
+    assert response.status_code == 200
+    assert response.json() == {"detail": "Robot created succesfully."}
+
+
+# Creating Robot with avatar
+
+def test_create_robot_avatar():
+    new_robot_upl = {
+        "robot_name": (get_random_string_lower(5)),
+        "creator": 1
+    }
+    code = open("Tests/files/dummybot.py", "rb")
+    avatar = open("Tests/files/image.jpg", "rb")
+    response = client.post("/robot/create", params = new_robot_upl, files={"code":code, "avatar":avatar})
     assert response.status_code == 200
     assert response.json() == {"detail": "Robot created succesfully."}
 
@@ -27,11 +41,10 @@ def test_create_robot():
 def test_create_robot_invalid_id():
     new_robot_upl_invalid_id = {
         "robot_name": (get_random_string_lower(5)),
-        "code": (get_random_string_lower(5)),
-        "avatar": (get_random_string_lower(5)),
         "creator": (get_rand_negative())
     }
-    response = client.post("/robot/create", json = new_robot_upl_invalid_id)
+    code = open("Tests/files/dummybot.py", "rb")
+    response = client.post("/robot/create", params = new_robot_upl_invalid_id, files={"code":code})
     assert response.status_code == 404
     assert response.json() == {"detail": "There is no user with such ID."}
 
@@ -40,10 +53,9 @@ def test_create_robot_invalid_id():
 def test_create_robt_inv_name():
     new_robot_upl_inv_name = {
         "robot_name": (get_random_string_special()),
-        "code": (get_random_string_lower(5)),
-        "avatar": (get_random_string_lower(5)),
         "creator": 1
     }
-    response = client.post("/robot/create", json = new_robot_upl_inv_name)
+    code = open("Tests/files/dummybot.py", "rb")
+    response = client.post("/robot/create", params = new_robot_upl_inv_name, files={"code":code})
     assert response.status_code == 400
     assert response.json() == {"detail": "Invalid robot name."}
