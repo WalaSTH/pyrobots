970e7c2c008b43790e40aad240a54d6249278ce4
diegogimenez04 <d.gimenez0101@gmail.com>
2022-10-18 00:38:24 -0300

    Improved data manipulation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./screens/Login/Login";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
