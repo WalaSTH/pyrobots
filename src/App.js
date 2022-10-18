af6d197ee350f4dab1bc8f64cfd6d18df5f1c3da
MartinPiloni <maartinpiloni@gmail.com>
2022-10-18 07:39:05 -0300

    Send creator ID to backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./screens/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
 import CreateMatch from "./screens/CreateMatch/CreateMatch";
 
 export default function App() {
+  const id = 1;
   return (
     <div className="App">
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-match" element={<CreateMatch />} />
+        <Route path="/" element={<></>} />
+        <Route path="/create-match" element={<CreateMatch userID={id} />} />
       </Routes>
     </div>
   );
