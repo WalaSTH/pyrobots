cf1154950d3f800bf7b16952e6e46f8c9184ce25
germansola <solagerman00@gmail.com>
2022-10-17 23:20:58 -0300

    returns nuevos para login

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     access_token = create_access_token(
         data={"sub": user.user_name}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", "username": user.user_name,"id":user.id}
 
 
 @app.get("/user/me/")
--- pydantic_models.py
+++ pydantic_models.py
@@ -15,8 +15,8 @@ class UserTemp(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
-
-
+    username: str
+    id: int
 class TokenData(BaseModel):
     username: Optional[str] = None
 
