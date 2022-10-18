a368e338045ae1bab426b90c84ec8a120aceca4e
Francisco Percivaldi <franpercivaldi@gmail.com>
2022-10-18 07:36:30 -0300

    Fix merge(cont)

--- src/components/FormsUI/Textfield/index.js
+++ src/components/FormsUI/Textfield/index.js
@@ -2,19 +2,21 @@ import React from "react";
 import { TextField } from "@mui/material";
 import { useField } from "formik";
 
-export default function TextfieldWrapper({ name, ...otherProps }) {
+export default function TextFieldWrapper({ name, ...otherProps }) {
   const [field, meta] = useField(name);
 
-  const configTextfield = {
+  const configTextField = {
     ...field,
     ...otherProps,
     fullWidth: true,
     variant: "outlined",
+    margin: "dense",
   };
 
   if (meta && meta.touched && meta.error) {
-    configTextfield.error = true;
-    configTextfield.helperText = meta.error;
+    configTextField.error = true;
+    configTextField.helperText = meta.error;
   }
-  return <TextField {...configTextfield} />;
+
+  return <TextField {...configTextField} />;
 }
