84d86ee9b989ce4e6b56982264b29ddac4cea1e0
Felipe Clariá <felipe.claria@mi.unc.edu.ar>
2022-10-24 14:53:03 -0300

    File input and upload button components

---
+++ src/components/FormsUI/FileUploadButton/index.js
@@ -0,0 +1,25 @@
+import { Button, FormHelperText } from "@mui/material";
+import { useField, useFormikContext } from "formik";
+
+export default function FileUpload({ name, children, ...otherProps }) {
+  const [, meta] = useField(name);
+  const isError = meta.touched && meta.error;
+
+  const configButton = {
+    component: "label",
+    htmlFor: name,
+    fullWidth: true,
+    variant: "outlined",
+    color: isError ? "error" : "primary",
+    ...otherProps,
+  };
+
+  return (
+    <>
+      <Button {...configButton}> {children} </Button>
+      {isError ? (
+        <FormHelperText error={true}> {meta.error} </FormHelperText>
+      ) : null}
+    </>
+  );
+}
