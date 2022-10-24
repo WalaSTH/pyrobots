e3cdc321559f9003e820677598483ddc29efe999
Felipe Clariá <felipe.claria@mi.unc.edu.ar>
2022-10-24 14:53:03 -0300

    File input and upload button components

---
+++ src/components/FormsUI/FileInput/index.js
@@ -0,0 +1,19 @@
+import { useField, useFormikContext } from "formik";
+
+export default function FileUpload({ name, ...otherProps }) {
+  const { setFieldValue } = useFormikContext();
+
+  function handleChange(e) {
+    setFieldValue(name, e.target.files[0]);
+  }
+
+  const configInput = {
+    id: name,
+    type: "file",
+    hidden: true,
+    onChange: handleChange,
+    ...otherProps,
+  };
+
+  return <input {...configInput} />;
+}
