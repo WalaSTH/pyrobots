80f1df3057f2aca896db9551fabdcc766b73718f
MartinPiloni <maartinpiloni@gmail.com>
2022-10-27 11:31:31 -0300

    Fix responsive design in login page

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,12 +1,8 @@
 import React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import { Card } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import * as Yup from "yup";
 import { Formik, Form } from "formik";
 import TextField from "../FormsUI/TextField";
@@ -29,80 +25,65 @@ const formValidation = Yup.object().shape({
 });
 
 export default function LoginForm({ handleSubmit }) {
-  const theme = createTheme();
-
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            marginBottom: "350px",
-            marginTop: "150px",
-          }}
-        >
-          <Formik
-            initialValues={{ ...initialFormState }}
-            validationSchema={formValidation}
-            onSubmit={handleSubmit}
-          >
-            <Form>
-              <Card
-                variant="outlined"
-                sx={{
-                  marginTop: 3,
-                  padding: 3,
-                  borderRadius: 3,
-                  textAlign: "center",
-                }}
+    <Formik
+      initialValues={{ ...initialFormState }}
+      validationSchema={formValidation}
+      onSubmit={handleSubmit}
+    >
+      <Card
+        variant="outlined"
+        sx={{
+          marginTop: 3,
+          padding: 3,
+          borderRadius: 3,
+          textAlign: "center",
+        }}
+      >
+        <Form>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography
+                component="h1"
+                variant="h5"
+                data-testid="typography-signin"
               >
-                <Typography
-                  component="h1"
-                  variant="h5"
-                  sx={{ color: "black", marginBottom: "20px" }}
-                  data-testid="typography-signin"
-                >
-                  Sign in
-                </Typography>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <TextField
-                      name="username"
-                      label="Username"
-                      autoComplete="off"
-                      data-testid="username-input"
-                    />
-                  </Grid>
+                Sign in
+              </Typography>
+            </Grid>
 
-                  <Grid item xs={12}>
-                    <TextField
-                      name="password"
-                      label="Password"
-                      type="password"
-                      data-testid="password-input"
-                    />
-                  </Grid>
+            <Grid item xs={12}>
+              <Textfield
+                name="username"
+                label="Username"
+                autoComplete="off"
+                data-testid="username-input"
+              />
+            </Grid>
 
-                  <Grid item xs={12}>
-                    <SubmitFormButton data-testid="login-button">
-                      Login
-                    </SubmitFormButton>
-                  </Grid>
+            <Grid item xs={12}>
+              <Textfield
+                name="password"
+                label="Password"
+                type="password"
+                data-testid="password-input"
+              />
+            </Grid>
 
-                  <Grid item xs={12}>
-                    <Link href="/register" variant="body2">
-                      {"Don't have an account? Sign Up"}
-                    </Link>
-                  </Grid>
-                </Grid>
-              </Card>
-            </Form>
-          </Formik>
-        </Box>
-      </Container>
-    </ThemeProvider>
+            <Grid item xs={12}>
+              <Button type="submit" data-testid="login-button">
+                Login
+              </Button>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Link href="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Form>
+      </Card>
+    </Formik>
   );
 }
