d2f9233ca7571a7e028c6e1426b63c96c6b2b7e2
Felipe Clariá <felipe.claria@mi.unc.edu.ar>
2022-10-29 00:48:03 -0300

    Hotfix form submit

--- src/components/FormsUI/SubmitFormButton/index.js
+++ src/components/FormsUI/SubmitFormButton/index.js
@@ -1,15 +1,11 @@
 import { Button } from "@mui/material";
-import { useFormikContext } from "formik";
 
 export default function ButtonWrapper({ children, ...otherProps }) {
-  const { submitForm } = useFormikContext();
-
   const configButton = {
     variant: "contained",
     color: "primary",
     size: "large",
     fullWidth: true,
-    onClick: submitForm,
     type: "submit",
     ...otherProps,
   };
