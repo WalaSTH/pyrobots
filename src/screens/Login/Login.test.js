93ba6edaade8c2042a38bd32bb0c7d9902f3e51a
diegogimenez04 <d.gimenez0101@gmail.com>
2022-11-03 00:10:09 -0300

    Login test snapshot

--- src/screens/Login/Login.test.js
+++ src/screens/Login/Login.test.js
@@ -1,34 +1,12 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import userEvent from "@testing-library/user-event";
 import renderer from "react-test-renderer";
-import { render, screen } from "@testing-library/react";
 import Login from "./Login";
 
 describe("<Login", () => {
-  const user = userEvent.setup();
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
-
   it("component rendering", async () => {
     const component = renderer.create(<Login />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
-  });
-
-  it("component renders with all the components", () => {
-    render(<Login />);
-  });
-
-  it("mock fetch", async () => {
-    const fakeUser = {
-      username: "diego",
-      password: "Diego123_",
-    };
-
-    // await Login.loginUser(fakeUser);
-    // expect(Login.handleSubmit).toHaveBeenCalledTimes(1);
-    // expect(Login.handleSubmit).toHaveBeenCalledWith(fakeUser);
   });
 });
