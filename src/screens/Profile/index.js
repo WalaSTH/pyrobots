a4be4db7e2c8b4601e3cb6ac1eef22da0d3cf09c
Felipe Clariá <felipe.claria@mi.unc.edu.ar>
2022-11-08 21:30:53 -0300

    Profile interface

---
+++ src/screens/Profile/index.js
@@ -0,0 +1,22 @@
+// import { Container } from "@mui/material";
+// import DisplayProfile from "../../components/DisplayProfile";
+
+import { Container } from "@mui/material";
+import ProfileDisplay from "../../components/ProfileDisplay";
+
+export default function Profile({}) {
+  const username = localStorage.getItem("username");
+  const avatar = localStorage.getItem("avatar");
+  const stats = {};
+  return (
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        marginTop: 10,
+      }}
+    >
+      <ProfileDisplay username={username} avatar={avatar} stats={stats} />
+    </Container>
+  );
+}
