5306b858ac428145eb1aa66eddd35278e2aed7d0
WalaSTH <macielsalguero@gmail.com>
2022-11-16 12:34:31 -0300

    robot modify

--- super_robot_copy.py
+++ super_robot_copy.py
@@ -1,8 +1,8 @@
 class SuperRobotCopy(gameRobot):
     def initialize(self):
         print("Initializing " + self.robot_name + ".")
-        count_var: int
-        count_var = 0
+        self.count_var: int
+        self.count_var = 0
         self.cannon_cooldown = 0
         self.anglee = 0
         self.counter = 0
@@ -12,8 +12,10 @@ class SuperRobotCopy(gameRobot):
             self.count = 0
             self.lastAngle = 0
             self.foundOne = False
+            self.set_position(100, 500)
             xpos = self.x_position
             ypos = self.y_position
+            self.found_far = False
             print(
                 "Position for "
                 + self.robot_name
@@ -23,6 +25,7 @@ class SuperRobotCopy(gameRobot):
                 + str(ypos)
             )
         if self.robot_name == "Copy2":
+            self.set_position(850, 500)
             xpos = self.x_position
             ypos = self.y_position
             print(
@@ -40,22 +43,32 @@ class SuperRobotCopy(gameRobot):
         if self.robot_name == "Copy1":
             if not self.foundOne:
                 if self.scanned() < 1000:
-                    print("Enemy scanned, so drive will be set to" + str(self.lastAngle))
-                    self.foundOne = True
-                    self.drive(self.lastAngle, 100)
-                else:
-                    self.point_scanner(self.anglee, 5)
+                    if self.scanned()<=700:
+                        self.foundOne = True
+                        self.enemy_scanned = self.scanned()
+                        self.cannon(self.lastAngle, self.enemy_scanned)
+                        print("Cannon activated to angle "  + str(self.lastAngle)+ "and distance " + str(self.scanned()))
+                    else:
+                        self.drive(self.lastAngle, 100)
+                        print("Not close enough so will move")
+                        self.foundOne = False
+                        self.found_far = True
+                elif self.found_far == False:
+                    self.point_scanner(self.anglee, 15)
                     print("Scanner pointed at " + str(self.anglee))
                     self.lastAngle = self.anglee
-                    self.anglee += 5
-        if self.robot_name == "Copy2":
-            self.counter = self.counter + 1
-            print("Drive will be set to (" + str(self.counter) + "," + str(100) + ")")
-            self.drive(self.counter, 100)
-        if self.robot_name == "Copy3":
-            if self.counter2 == 15:
-                self.drive(self.amount, 100)
-                self.amount = self.amount+1
+                    self.anglee += 10
+                else:
+                    self.point_scanner(self.lastAngle, 15)
             else:
-                self.drive(0, 100)
-                self.counter2+=1
+                #self.drive(self.lastAngle,50)
+                self.cannon(self.lastAngle, self.enemy_scanned)
+                print("Cannon activated to angle "  + str(self.lastAngle)+ "and distance " + str(self.enemy_scanned))
+                #pass
+        if self.robot_name == "Copy2":
+            self.counter += 10
+            #print("Drive will be set to (" + str(self.counter) + "," + str(100) + ")")
+
+
+            #self.drive(self.counter, 100)
+        
