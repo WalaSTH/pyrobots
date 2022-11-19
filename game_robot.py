982011bbbff455d77e1f981d4a41047d678e9121
WalaSTH <macielsalguero@gmail.com>
2022-11-19 19:30:30 -0300

    Scan is implemented

--- game_robot.py
+++ game_robot.py
@@ -32,33 +32,6 @@ FRAMES_FOR_CHANGE = 5
 SN = 5
 
 ### GAME CLASSES
-class Missile:
-    x_position: int
-    y_position: int
-    x_target: int
-    y_target: int
-    direction: int
-    distance: int
-    remains: int
-    def __init__(self, x_p,y_p,x_t,y_t, dir, dis, rem):
-        self.x_position = x_p
-        self.y_position = y_p
-        self.x_target = x_t
-        self.y_target = y_t
-        self.direction = dir
-        self.distance = dis
-        self.remains = rem
-
-class Explotion:
-    id_robot: int
-    game_id_robot: int
-    x_position: int
-    y_position: int
-    def __init__(self, id, game_id, x, y):
-        self.id_robot = id
-        self.game_id_robot = game_id
-        self.x_position = x
-        self.y_position = y
 
 class gameRobot:
     robot_id: int
@@ -81,14 +54,6 @@ class gameRobot:
     scan_result: int
     scan_len: int
     scan_setted: bool
-    # cannon
-    cannon_ready: bool
-    cannon_setted: bool
-    cannon_cooldown: int
-    cannon_direction: int
-    cannon_distance: int
-    # missiles
-    missiles: list[Missile]
 
     def __init__(self, id):
         self.robot_id = id
@@ -106,11 +71,6 @@ class gameRobot:
         self.scan_len = SCAN_LENGTH
         self.scan_setted = False
         self.scan_result = math.inf
-        # cannon inits
-        self.cannon_cooldown = 0
-        self.cannon_ready = True
-        self.cannon_setted = False
-        self.missiles = []
 
     def set_position(self, x, y):
         self.x_position = x
@@ -246,105 +206,15 @@ class gameRobot:
         else:
             self.scan_result = math.inf
 
-    def shoot_cannon(self):
-        if self.cannon_cooldown == 0 and self.cannon_setted:
-            self.cannon_cooldown = CANNON_COOLDOWN_DEFAULT
-            self.cannon_setted = False
-            ## Get target x y
-            y_target = get_y_add(self.cannon_distance, self.cannon_direction)
-            x_target = get_x_add(self.cannon_distance, self.cannon_direction)
-            x_target = (
-                self.x_position + x_target
-                if self.x_position + x_target < TABLE_HORIZONTAL_LENGHT
-                else TABLE_HORIZONTAL_LENGHT
-            )
-            if x_target < 0:
-                x_target = 0
-
-            y_target  = (
-                self.y_position + y_target
-                if self.y_position + y_target  < TABLE_VERTICAL_LENGHT
-                else TABLE_VERTICAL_LENGHT
-            )
-            if y_target  < 0:
-                y_target  = 0
-
-            new_missile = Missile(
-                self.x_position,
-                self.y_position,
-                x_target,
-                y_target,
-                self.cannon_direction,
-                self.cannon_distance,
-                rounds_to_explote(self.cannon_distance, MISSILE_SPEED)
-            )
-
-            self.missiles.append(new_missile)
-            print(
-                str(self.robot_name)
-                + " has fired a missle to direction "
-                + str(self.cannon_direction)
-                + " with distance "
-                + str(self.cannon_distance)
-            )
-        else:
-            self.cannon_cooldown = (
-                self.cannon_cooldown - 1 if self.cannon_cooldown - 1 > 0 else 0
-            )
-        self.cannon_setted = False
-
-    def missile_advance(self, explotion_list):
-        n_missiles = len(self.missiles)
-        for i in range(n_missiles):
-            missile_direction = self.missiles[i].direction
-            missile_distance = self.missiles[i].distance
-            # calcular nuevo punto x,y para el misil
-            y_add = get_y_add(MISSILE_SPEED, missile_direction)
-            x_add = get_x_add(MISSILE_SPEED, missile_direction)
-            # sumar y mover
-            self.missiles[i].x_position = (
-                self.missiles[i].x_position + x_add
-                if self.missiles[i].x_position + x_add < TABLE_HORIZONTAL_LENGHT
-                else TABLE_HORIZONTAL_LENGHT
-            )
-            if self.missiles[i].x_position < 0:
-                self.missiles[i].x_position = 0
-
-            self.missiles[i].y_position = (
-                self.missiles[i].y_position + y_add
-                if self.missiles[i].y_position + y_add < TABLE_VERTICAL_LENGHT
-                else TABLE_VERTICAL_LENGHT
-            )
-            if self.missiles[i].y_position < 0:
-                self.missiles[i].y_position = 0
-            print("Missile is in (" + str(self.missiles[i].x_position) + ", "+str(self.missiles[i].y_position) + ").")
-            # ver si explota
-            self.missiles[i].remains = self.missiles[i].remains - 1 if self.missiles[i].remains - 1 > 0 else 0
-            if self.missiles[i].remains == 0:
-                # explotar misil
-                new_explotion = Explotion(self.robot_id, self.game_id_robot, self.missiles[i].x_target, self.missiles[i].y_target)
-                explotion_list.append(new_explotion)
-                self.missiles.remove(self.missiles[i])
-                print("Missile exploted")
-
-    def deal_damage(self, damage):
-        self.health = self.health - damage if self.health - damage > 0 else 0
-        print("dealed "+str(damage)+ "to robot")
     ###### User accesible methods
 
     # Cannon
 
     def is_cannon_ready(self):
-        return self.cannon_cooldown == 0
+        return True
 
     def cannon(self, degree, distance):
-        self.cannon_direction = get_angle(degree)
-        if distance < 0:
-            distance = 0
-        elif distance > MISSILE_MAX_DISTANCE:
-            distance = MISSILE_MAX_DISTANCE
-        self.cannon_distance = distance
-        self.cannon_setted = True
+        return True
 
 
     # Scan
