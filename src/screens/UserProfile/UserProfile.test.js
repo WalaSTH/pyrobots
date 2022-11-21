0537f2ff6dddaa8ea5ded63d33d15f1e1463d2ae
feliclaria <felipe.claria@mi.unc.edu.ar>
2022-11-21 14:56:26 -0300

    Test axios responses

--- src/screens/UserProfile/UserProfile.test.js
+++ src/screens/UserProfile/UserProfile.test.js
@@ -1,102 +1,60 @@
-import axios from "axios";
 import "@testing-library/jest-dom/extend-expect";
-import userEvent from "@testing-library/user-event";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import renderer, { act } from "react-test-renderer";
 import UserProfile from ".";
+import axios from "axios";
 
 jest.mock("axios");
 
 describe("UserProfile", () => {
-  // const user = userEvent.setup();
-  afterEach(() => {
-    jest.restoreAllMocks();
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  it("should render the component", () => {
-    axios.get.mockImplementationOnce(() => Promise.resolve());
+  it("should render correct stats after fethching succesfully", async () => {
+    const mockedStats = { played_matches: 1, victories: 1 };
+
+    axios.get.mockResolvedValue({
+      data: { stats: mockedStats },
+    });
+
     render(<UserProfile />);
-    expect(screen.getByTestId("userAvatar")).toBeInTheDocument();
-    expect(screen.getByTestId("userName")).toBeInTheDocument();
-    expect(screen.getByTestId("userSettingsButton")).toBeInTheDocument();
-    expect(screen.getByText(/User stats/i)).toBeInTheDocument();
-    expect(screen.getByText(/Matches played/i)).toBeInTheDocument();
-    expect(screen.getByText(/Matches won/i)).toBeInTheDocument();
-    expect(screen.getByText(/Victory rate/i)).toBeInTheDocument();
-  });
 
-  it("should render user stats after succesful fetch", async () => {
-    const mockAxiosResponse = {
-      status: 200,
-      data: { played_matches: 1, victories: 1 },
-    };
-    axios.get.mockImplementation(() => Promise.resolve(mockAxiosResponse));
+    await waitFor(() => {
+      const statsItems = screen.queryAllByTestId("statsItem");
+      const statsSkeletons = screen.queryAllByTestId("statsSkeleton");
 
-    act(() => {
-      render(<UserProfile />);
-    });
+      expect(statsItems).toHaveLength(3);
+      expect(statsSkeletons).toHaveLength(0);
 
-    expect(screen.getByTestId("statsPlayed")).toHaveTextContent(
-      mockAxiosResponse.data.played_matches
-    );
-
-    expect(screen.getByTestId("statsWon")).toHaveTextContent(
-      mockAxiosResponse.data.victories
-    );
-
-    expect(screen.getByTestId("statsRate")).toHaveTextContent(
-      mockAxiosResponse.data.victories / mockAxiosResponse.data.played_matches +
-        "%"
-    );
-  });
-
-  it("should render Skeletons after unsuccesful fetch", async () => {
-    const mockAxiosError = {
-      detail: "dummy error message",
-    };
-
-    jest
-      .mocked(axios.get)
-      .mockImplementation(() =>
-        Promise.reject({ detail: "dummy error message" })
+      expect(statsItems[0]).toHaveTextContent(mockedStats.played_matches);
+      expect(statsItems[1]).toBeInTheDocument(mockedStats.victories);
+      expect(statsItems[2]).toBeInTheDocument(
+        mockedStats.victories / mockedStats.played_matches + "%"
       );
-
-    act(() => {
-      render(<UserProfile />);
     });
-    expect(screen.getAllByTestId("statsSkeleton")).toHaveLength(3);
-    expect(screen.getByTestId("profileSnackbar")).toHaveTextContent(
-      mockAxiosError.detail
-    );
   });
 
-  // it("should render menu after click on user settings button", async () => {
-  //   render(<UserProfile />);
-  //   const userSettingsButton = screen.getByTestId("userSettingsButton");
-  //   await user.click(userSettingsButton);
+  it("should render skeletons and snackbar after fetching erroneously", async () => {
+    const mockedDetail = "dummy error message";
 
-  //   const userSettingsMenu = screen.getByTestId("userSettingsMenu");
-  //   const avatarMenuItem = screen.getByText("Change user avatar");
-  //   const passwordMenuItem = screen.getByText("Change password");
+    axios.get.mockRejectedValue({
+      response: { data: { detail: mockedDetail } },
+    });
 
-  //   expect(userSettingsMenu).toBeInTheDocument();
-  //   expect(userSettingsMenu).toContainElement(avatarMenuItem);
-  //   expect(userSettingsMenu).toContainElement(passwordMenuItem);
-  // });
+    render(<UserProfile />);
 
-  //   it("should stop rendering menu after clicking away", async () => {
-  //     const user = userEvent.setup();
-  //     render(<UserProfile />);
-  //     const userSettingsButton = screen.getByTestId("userSettingsButton");
+    await waitFor(() => {
+      const statsItems = screen.queryAllByTestId("statsItem");
+      const statsSkeletons = screen.queryAllByTestId("statsSkeleton");
+      const errorSkeleton = screen.getByTestId("profileSnackbar");
 
-  //     await user.click(userSettingsButton);
-  //     expect(screen.getByTestId("userSettingsMenu")).toBeInTheDocument();
-
-  //     fireEvent.click(screen.getByText("Change user avatar"));
-  //     await waitFor(() => {
-  //       expect(screen.getByTestId("userSettingsMenu")).toBeInTheDocument();
-  //     });
-  //   });
+      expect(statsItems).toHaveLength(0);
+      expect(statsSkeletons).toHaveLength(3);
+      expect(errorSkeleton).toBeInTheDocument();
+      expect(errorSkeleton).toHaveTextContent(mockedDetail);
+    });
+  });
 
   it("matches Snapshot", () => {
     const component = renderer.create(<UserProfile />);
