0b64937147022abd5d13dbcc6b92a5b0009f1e0b
feliclaria <felipe.claria@mi.unc.edu.ar>
2022-11-21 16:19:31 -0300

    Test component and menu rendering

--- src/screens/UserProfile/UserProfile.test.js
+++ src/screens/UserProfile/UserProfile.test.js
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import renderer, { act } from "react-test-renderer";
 import UserProfile from ".";
 import axios from "axios";
@@ -11,7 +12,30 @@ describe("UserProfile", () => {
     jest.clearAllMocks();
   });
 
-  it("should render correct stats after fethching succesfully", async () => {
+  it("should render initial state of the component correctly", async () => {
+    await act(async () => render(<UserProfile />));
+    // User info
+    expect(screen.getByTestId("userAvatar")).toBeInTheDocument();
+    expect(screen.getByTestId("userName")).toBeInTheDocument();
+
+    // User settings button
+    expect(screen.getByTestId("userSettingsButton")).toBeInTheDocument();
+
+    // User stats
+    expect(screen.getByText(/User stats/i)).toBeInTheDocument();
+    expect(screen.getByText(/Matches played/i)).toBeInTheDocument();
+    expect(screen.getByText(/Matches won/i)).toBeInTheDocument();
+    expect(screen.getByText(/Victory rate/i)).toBeInTheDocument();
+    expect(screen.queryAllByTestId("statsSkeleton")).toHaveLength(3);
+
+    // Settings menu should be closed
+    expect(screen.queryByTestId("userSettingsMenu")).toBeNull();
+    expect(screen.queryAllByTestId("menuItem")).toHaveLength(0);
+    expect(screen.queryByText(/Change user avatar/i)).toBeNull();
+    expect(screen.queryByText(/Change password/i)).toBeNull();
+  });
+
+  it("should render stats after succesfully fethching stats", async () => {
     const mockedStats = { played_matches: 1, victories: 1 };
 
     axios.get.mockResolvedValue({
@@ -35,7 +59,7 @@ describe("UserProfile", () => {
     });
   });
 
-  it("should render skeletons and snackbar after fetching erroneously", async () => {
+  it("should render skeletons and snackbar after erroneously fetching stats", async () => {
     const mockedDetail = "dummy error message";
 
     axios.get.mockRejectedValue({
@@ -54,6 +78,35 @@ describe("UserProfile", () => {
       expect(errorSkeleton).toBeInTheDocument();
       expect(errorSkeleton).toHaveTextContent(mockedDetail);
     });
+  });
+
+  it("should open menu after clicking on the settings icon", async () => {
+    const user = userEvent.setup();
+    await act(async () => render(<UserProfile />));
+
+    await user.click(screen.getByTestId("userSettingsButton"));
+
+    expect(screen.getByTestId("userSettingsMenu")).toBeInTheDocument();
+    expect(screen.queryAllByTestId("menuItem")).toHaveLength(2);
+    expect(screen.getByText(/Change user avatar/i)).toBeInTheDocument();
+    expect(screen.getByText(/Change password/i)).toBeInTheDocument();
+  });
+
+  it("should close menu after clicking on any of the options", async () => {
+    const user = userEvent.setup();
+    await act(async () => render(<UserProfile />));
+
+    await user.click(screen.getByTestId("userSettingsButton"));
+    user.click(screen.getByText(/Change user avatar/i));
+    await waitFor(() =>
+      expect(screen.queryByTestId("userSettingsMenu")).toBeNull()
+    );
+
+    await user.click(screen.getByTestId("userSettingsButton"));
+    user.click(screen.getByText(/Change password/i));
+    await waitFor(() =>
+      expect(screen.queryByTestId("userSettingsMenu")).toBeNull()
+    );
   });
 
   it("matches Snapshot", () => {
